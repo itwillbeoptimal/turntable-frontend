598775d7b1aca5e148dbc14dfcbf8ab149783584
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-10-02 00:56:12 +0900

    config: ESLint(Airbnb 스타일) 설정 및 Prettier 적용
    - ref: #1

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import MyIcon from './assets/react.svg?react';
 
 function App() {
   return (
-    <>
     <div>
-        <MyIcon width={40} height={40} />
+      <MyIcon width={40} height={40} />
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
